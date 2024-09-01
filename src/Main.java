ebc642db83d95eb10654927a90cd79043b831caa
Dessalew Akassa <dessalew25@gmail.com>
2024-09-01 15:28:02 -0700

    Nested Loop in Java

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ public class Main {
 
         //Printing a Traingle using Nested loop
 
-        for (int i =1;i<6;i++){
-            for (int j =1;j<i;j++){
+        for (int i =1;i<=6;i++){
+            for (int j =1;j<=i;j++){
 
                 System.out.print("*");
 
