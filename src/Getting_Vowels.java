fe817963ee1beacc669e46568e904da9dd261c59
Dessa19 <dessalew25@gmail.com>
2024-09-23 13:11:56 -0700

    getting the number of vowels from word and sentence1

--- src/Getting_Vowels.java
+++ src/Getting_Vowels.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Getting_Vowels {
     public static void main(String[] args) {
-
+// Print out Vowels using Scanner Class.
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a String");
         String  string = scanner.nextLine();
@@ -16,6 +16,21 @@ public class Getting_Vowels {
            if (ch =='a' || ch == 'e' ||ch == 'i'|| ch == 'o' || ch == 'u')
                vowel++;
         }
-        System.out.println("total number of Vowels are " + vowel);
+        System.out.print("total number of Vowels are " + vowel);
+        System.out.println("\n");
+
+        System.out.println("the number of Vowels from this sentence  are : " + getVowels(" my friend how are you"));
     }
+    //Print out vowels using method
+    public static int getVowels(String str){
+        int vowels = 0;
+        for ( int i =0; i<str.length(); i++){
+            if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'o' || str.charAt(i) == 'u'){
+                vowels++;
+
+            }
+        }
+        return vowels;
+    }
+
 }
